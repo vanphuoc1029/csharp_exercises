1da06c4758c467d522e0acdf43814805b42be679
phuochv <141982814+phuochv@users.noreply.github.com>
2023-08-11 10:33:17 +0700

    Create vphuoc.cs

---
+++ bai_4/vphuoc.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap N: ");
+            int N = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (float i = 1; i <= N; i++)
+            {
+                sum += 1 / (2*i);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
