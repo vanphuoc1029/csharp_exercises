ca748351147733c43417f54ef4b787a199e5f945
Ha Van Phuoc <vanphuoc1029@gmail.com>
2023-08-12 08:50:37 +0700

    add bai 7

---
+++ bai_7/phuoc.cs
@@ -0,0 +1,25 @@
+// Bài 7: Tính S(n) = ½ +2 / 3 + ¾ + …. +n / n + 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap N: ");
+            int N = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (float i = 1; i <= N; i++)
+            {
+                sum += i / (i + 1);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
