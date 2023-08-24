b7ba7f57193b6fdf6605066061b5d91e60641257
vanphuoc1029 <111067471+vanphuoc1029@users.noreply.github.com>
2023-08-24 17:45:29 +0700

    Create vphuoc.cs

---
+++ 034. Bài 34/vphuoc.cs
@@ -0,0 +1,28 @@
+//Bài 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn
+
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            double s = 0;
+            int gt = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                s = Math.Sqrt(i + s);
+            }
+            Console.WriteLine($"S: {s}");
+        }
+    }
+} 
