883890c6cc6b01632101ed0b21974c35f4ccc1b0
vanphuoc1029 <111067471+vanphuoc1029@users.noreply.github.com>
2023-08-24 17:25:39 +0700

    Update vphuoc.cs

--- 032. Bài 32/vphuoc.cs
+++ 032. Bài 32/vphuoc.cs
@@ -1,3 +1,4 @@
+//Bài 32: Cho số nguyên dương n. Kiểm tra xem n có phải là số chính phương hay không
 
 using Microsoft.Win32;
 using System;
@@ -16,21 +17,23 @@ namespace ConsoleApp1
             Console.Write("Nhap n: ");
             int n = int.Parse(Console.ReadLine());
             int count = 0;
-            for(int i = 2; i < n; i++)
+            bool result = false;
+            for(int i = 1; i < n; i++)
             {
-                if (n%i == 0)
+                if (i * i == n)
                 {
-                    count ++; 
-                   
-                }    
+                    result = true;
+                    break;
+                }
+
             }
-            if (count == 0)
+            if (result)
             {
-                Console.WriteLine($"{n} la so nguyen to");
+                Console.WriteLine($"{n} la so chinh phuong");
             }
             else
             {
-                Console.WriteLine($"{n} khong phai la so nguyen to");
+                Console.WriteLine($"{n} khong phai la so chinh phuong");
             }
         }
     }
