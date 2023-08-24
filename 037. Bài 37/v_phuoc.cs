51d91c909f867053e95b48ccf6239ca64c3e7af6
vanphuoc1029 <111067471+vanphuoc1029@users.noreply.github.com>
2023-08-24 17:55:45 +0700

    Create v_phuoc.cs

---
+++ 037. Bài 37/v_phuoc.cs
@@ -0,0 +1,27 @@
+//Bài 37: Tính S(n) = CanBac N(N + CanBac N – 1(N – 1 + … + CanBac3(3 + CanBac2(2))) có n – 1 dấu căn
+
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            double s = 0;
+            for(float i = 2; i <= n; i++)
+            {
+                s = Math.Pow(i + s, 1 / i) ;
+            }
+            Console.WriteLine($"S: {s}");
+        }
+    }
+} 
